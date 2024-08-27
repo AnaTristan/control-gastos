7c15e4a3c279f1f5e37cfb9995a830f4d36bc8e3
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-08-26 21:32:56 -0600

    Agregando archivo para reducer / Adding reducer file

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,26 @@
+export type BudgetActions = {
+  type: "add-budget";
+  payload: { budget: number };
+};
+
+export type BudgetState = {
+  budget: number;
+};
+
+export const InitialState: BudgetState = {
+  budget: 0,
+};
+
+export const BudgetReducer = (
+  state: BudgetState = InitialState,
+  action: BudgetActions
+) => {
+  if (action.type === "add-budget") {
+    return {
+      ...state,
+      budget: action.payload.budget,
+    };
+  }
+
+  return state;
+};
