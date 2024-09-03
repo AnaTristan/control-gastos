964bfae5c819190e67cb12b0313da480132afb58
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-02 21:24:26 -0600

    Agregando datepicker / adding datepicker to expense form

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
+  const [value, onChange] = useState<Value>(new Date());
+
   return (
     <form className="space-y-5">
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -46,6 +55,17 @@ const ExpenseForm = () => {
             </option>
           ))}
         </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha:
+        </label>
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          onChange={onChange}
+          value={value}
+        />
       </div>
 
       <input
