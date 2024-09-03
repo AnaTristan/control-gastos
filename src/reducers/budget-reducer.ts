4ab285acce48a063e4f5b2ef347e5afcb89b293e
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-02 19:37:53 -0600

    Agregando cierre y apertura dinamico en modal / Added dinamyc open and close modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,14 +1,19 @@
-export type BudgetActions = {
-  type: "add-budget";
-  payload: { budget: number };
-};
+export type BudgetActions =
+  | {
+      type: "add-budget";
+      payload: { budget: number };
+    }
+  | { type: "show-modal" }
+  | { type: "hide-modal" };
 
 export type BudgetState = {
   budget: number;
+  modal: boolean;
 };
 
 export const InitialState: BudgetState = {
   budget: 0,
+  modal: false,
 };
 
 export const BudgetReducer = (
@@ -19,6 +24,20 @@ export const BudgetReducer = (
     return {
       ...state,
       budget: action.payload.budget,
+    };
+  }
+
+  if (action.type === "show-modal") {
+    return {
+      ...state,
+      modal: true,
+    };
+  }
+
+  if (action.type === "hide-modal") {
+    return {
+      ...state,
+      modal: false,
     };
   }
 
