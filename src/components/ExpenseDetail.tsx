56a3344395f7b508b51c11b0bda02948febff52b
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-04 21:41:15 -0600

    Agregando expense item

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,15 +1,26 @@
+import { formatDate } from "../helpers";
 import { Expense } from "../types";
+import AmountDisplay from "./AmountDisplay";
 
 type ExpenseDetailProps = {
   expense: Expense;
 };
 
 const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
+  return (
+    <div className="bg-white shadow-lg p-10 border-b border-gray-200 flex gap-5 items-center">
+      <div></div>
 
-  return <div>
+      <div>
+        <p>{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">
+          {formatDate(expense.date!.toString())}
+        </p>
+      </div>
 
-    
-  </div>;
+      <AmountDisplay amount={expense.amount} />
+    </div>
+  );
 };
 
 export default ExpenseDetail;
