85da6b2e28c2991378b408bdd5165f31ddd88350
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-04 20:16:06 -0600

    Agregando componenetes vacios para msotrar gastos / adding empty components for showing expenses list

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,15 @@
+import { Expense } from "../types";
+
+type ExpenseDetailProps = {
+  expense: Expense;
+};
+
+const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
+
+  return <div>
+
+    
+  </div>;
+};
+
+export default ExpenseDetail;
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
+import ExpenseDetail from "./ExpenseDetail";
+
+const ExpenseList = () => {
+  const { state } = useBudget();
+
+  const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+
+  return (
+    <div className="mt-10">
+      {isEmpty ? (
+        <p className="text-gray-500 text-2xl font-bold"> No hay gastos</p>
+      ) : (
+        <>
+          {state.expenses.map((expense) => (
+            <ExpenseDetail key={expense.id} expense={expense} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ExpenseList;
