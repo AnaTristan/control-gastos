e47b65ecb6e7f26dab5e676c9d3baf00eb5b8b11
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-07 14:55:34 -0600

    correccion de clase de tailwind / fixing tailwind grammar

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         </h1>
       </header>
 
-      <div className="maxw-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <Budgetracker /> : <BudgetForm />}
       </div>
 
