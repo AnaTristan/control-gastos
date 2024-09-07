55cb972b36816f3dccd992b48f2f77e4ee559575
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-07 14:56:08 -0600

    Agregando texto para lista de gastos / adding text for showing expense list

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -8,11 +8,14 @@ const ExpenseList = () => {
   const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
 
   return (
-    <div className="mt-10">
+    <div>
       {isEmpty ? (
-        <p className="text-gray-500 text-2xl font-bold"> No hay gastos</p>
+        <p className="text-gray-500 text-2xl font-bold ml-10"> No hay gastos</p>
       ) : (
         <>
+          <p className="text-2xl mx-10 my-6 font-bold text-gray-500">
+            Listado de gastos
+          </p>
           {state.expenses.map((expense) => (
             <ExpenseDetail key={expense.id} expense={expense} />
           ))}
