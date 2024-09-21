161e57bf36cce9117e87cebb98134aa3c17de0fd
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-09-21 14:13:28 -0600

    Condicionando texto para edicion-nuevo  / conditionally showing modal title if expense is new or is editing expense

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -24,8 +24,6 @@ const ExpenseForm = () => {
         (expense) => expense.id === state.editingId
       )[0];
 
-      console.log("editedElement", editedElement);
-
       setExpense(editedElement);
     }
   }, [state.editingId]);
@@ -70,7 +68,7 @@ const ExpenseForm = () => {
   return (
     <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
-        Nuevo Gasto
+        {state.editingId ? "Editar gasto" : "Nuevo gasto"}
       </legend>
 
       {error && <ErrorMessage>{error}</ErrorMessage>}
@@ -140,7 +138,7 @@ const ExpenseForm = () => {
       <input
         type="submit"
         className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-        value={"Registrar Gasto"}
+        value={state.editingId ? "Guardar cambios" : "Registrar gasto"}
       />
     </form>
   );
